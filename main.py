03e7a6687727e856ed2868561b1975b389da9850
Aleksandra Vorobeva <V-Sash@mail.ru>
2020-12-12 18:49:24 +0300

    обновила информацию

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import telebot
 from telebot import types
 
 
-bot = telebot.TeleBot('*******')
+bot = telebot.TeleBot('891829442:AAF17IVzOFGrEG5Pc3tN9vURdgcCMmVBiwI')
 
 keyboard1 = telebot.types.ReplyKeyboardMarkup()
 keyboard1.row('информация', 'связь')
 keyboard2 = telebot.types.ReplyKeyboardMarkup()
-keyboard2.row('да, давай пообщаемся')
+keyboard2.row('давай общаться')
 
 
 @bot.message_handler(commands=['start'])
@@ -25,24 +25,23 @@ def instagram(message):
 	markup.add(types.InlineKeyboardButton("Перейти в Инстаграм", url="https://www.instagram.com/sparkling_acidity/"))
 	bot.send_message(message.chat.id, "В Инстаграме у меня такой же ник, как и в телеге @sparkling_acidity.\nМожешь просто нажать на кнопку ниже и сразу попадешь в мой профиль", parse_mode='html', reply_markup=markup)
 
-@bot.message_handler(commands=['vk'])
+@bot.message_handler(commands=['git'])
 def vk(message):
 	markup = types.InlineKeyboardMarkup()
-	markup.add(types.InlineKeyboardButton("Перейти на мою страницу ВKонтакте", url="https://vk.com/inspiredbydream"))
-	bot.send_message(message.chat.id, "ВKонтакте я отвечаю только на сообщения по работе и учебе, просьба лищний раз не беспокоить.\n"
-                                      "По всем вопросам лучше писать в телеграм @sparkling_acidity или на эмейл V-Sash@mail.ru\n", parse_mode='html', reply_markup=markup)
+	markup.add(types.InlineKeyboardButton("Перейти на мой Git Hub", url="https://github.com/SparklingAcidity"))
+	bot.send_message(message.chat.id, "Здесь можно посмотреть мои лучшие готовые проекты.", parse_mode='html', reply_markup=markup)
 
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
     if message.text.lower() == 'информация':
-        bot.send_message(message.chat.id, 'Я путешественик и начинающий программист. Это, пожалуй, основное, что стоит знать.\n'
-                                          'Но можем пообщаться, хочешь?',reply_markup=keyboard2)
-    elif message.text.lower() == 'связь' or 'да, давай пообщаемся':
+        bot.send_message(message.chat.id, 'Я начинаюший веб разработчик на Питоне.\n'
+                                          'Ты можешь выбрать любой удобный тебе способ связи',reply_markup=keyboard2)
+    elif message.text.lower() == 'связь' or 'давай общаться':
         bot.send_message(message.chat.id, 'Мой телеграм @sparkling_acidity\n'
                                           'E-mail: V-Sash@mail.ru\n'
                                           'Инстаграм: /insta\n'
-                                          'ВК: /vk')
+                                          'Git Hub: /git')
 
 
 
